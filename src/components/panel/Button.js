5600b19b2c86094a5ebfda7d5f73af90efafed51
Tim Arney <tim@line37.com>
2022-08-23 11:22:10 -0400

    cleanup

--- src/components/panel/Button.js
+++ src/components/panel/Button.js
@@ -4,8 +4,9 @@ const StyledButton = styled.button`
     display:inline-flex;
     justify-content:center;
     align-items:center;
-    background-color: #fff;
     border:none;
+    background: ${props => props.isPrimary ? "#007cba" : "white"};
+    color: ${props => props.isPrimary ? "white" : "#000"};
 `;
 
 export const Button = ({ children, icon, onClick }) => {
